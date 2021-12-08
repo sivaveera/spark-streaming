9db8a56616646b284cdf5685b56245899dcecdd3
SivaVeera <sivaveera@gmail.com>
2021-12-07 17:10:09 -0800

    Update SubmitSparkStreamJob.py

--- DL_Pipelines/src/jobs/stream/SubmitSparkStreamJob.py
+++ DL_Pipelines/src/jobs/stream/SubmitSparkStreamJob.py
@@ -4,7 +4,7 @@ import requests
 import os
 from pathlib import Path
 
-DOMAIN = 'skechers-dev.cloud.databricks.com'
+DOMAIN = 'test-dev.cloud.databricks.com'
 TOKEN = os.environ['DB_TOKEN']
 BASE_URL = 'https://%s/api/2.0/jobs/runs/submit' % DOMAIN
 
